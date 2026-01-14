191e86f71c3132ef361df84e305868ab0c0d1a27
ehsanyaqoob <ehsanyaqoob07@gmail.com>
2026-01-14 20:57:09 +0500

    feat: tip calculator app

--- tip/ContentView.swift
+++ tip/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    // var for total amount
+    @State var total = ""
+    @State var tipPercent  = 15.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Image(systemName: "dollarsign.circle.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .font(.title)
+                Text("Tip Calculator App!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+            }
+            
+            HStack{
+                Text("$")
+                TextField("Amount", text: $total)
+                    .keyboardType(.decimalPad)
+            }
+            HStack{
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))")
+                Text("%")
+            }
+            if let totalNum = Double(total) {
+                let tip = totalNum * tipPercent * 0.01
+                Text("Tip Amount is: $\(tip, specifier: "%.2f")")
+            }
+            else{
+                Text("Please enter the Numeric Value for Amount.")
+            }
         }
         .padding()
     }
